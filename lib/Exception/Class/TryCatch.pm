434f8915b842adef58db76c74d6352a3f9884910
David Golden <dagolden@cpan.org>
2005-06-09 01:29:54 +0000

    Import of DAGOLDEN/Exception-Class-TryCatch-1.06 from CPAN.
    gitpan-cpan-distribution: Exception-Class-TryCatch
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         DAGOLDEN/Exception-Class-TryCatch-1.06.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Exception/Class/TryCatch.pm
+++ lib/Exception/Class/TryCatch.pm
@@ -7,7 +7,7 @@ use Exception::Class;
 BEGIN {
     use Exporter ();
     use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = "1.05";
+    $VERSION     = "1.06";
     @ISA         = qw (Exporter);
     @EXPORT      = qw ( catch try caught );
     @EXPORT_OK   = ();
